ada9e8d790cd57722748ccb462a2a00dbea72920
thepowerKey888 <thepower674@gmail.com>
2024-09-21 12:06:57 -0500

    Modified the SlayTheSpire.java file. Added a List to include all invalid cards. Made the HashMap accessible to other methods. Added checks for invalid cards. Began planning out the generateFile method to generate the report or VOID report.

--- src/SlayTheSpire.java
+++ src/SlayTheSpire.java
@@ -8,8 +8,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SlayTheSpire {
+
+    static List<String> invalid_cards = new ArrayList<>();
+    static HashMap<String, Integer> cardDeck = new HashMap<>();
 
 
     /**
@@ -20,43 +25,55 @@ public class SlayTheSpire {
      * @param filePath
      * @return
      */
-    public static HashMap<String, Integer>  readTxtFile(String filePath) {
-        HashMap<String, Integer> cardDeck = new HashMap<>();
+    public static boolean  readTxtFile(String filePath) {
+
         BufferedReader reader = null;
 
         try {
             //read file
             reader = new BufferedReader(new FileReader(filePath));
             String line;
+            int line_counter = 0;
 
             //read file line by line
             while ((line = reader.readLine()) != null) {
-                line = line.replaceAll("\\s+", ""); //check for white spaces in string
+                line_counter =+ 1; //counts the amount of lines in the txt file
+                if(line_counter > 1000){
+                    return false;
+                }
+
+                if( invalid_cards.size() > 10){
+                    return false;
+                }
+
+                line = line.replaceAll("\\s+", ""); //remove white spaces in string
+                line = line.toLowerCase(); //remove uppercase
+
+                if (line.isEmpty()){ //checks if string is empty
+                    invalid_cards.add(line); //adds to invalid_cards if true
+                }
 
                 if (line == null || !line.contains(":")) { //checks for invalid format
-                    cardDeck.put("Invalid Format", null);
+                    invalid_cards.add(line); //adds invalid card to invalid_card list
                 }
-                String[] str_and_int = line.split(":");
+
+                String[] str_and_int = line.split(":"); //split at ":"
 
                 if(str_and_int.length == 2 && isInteger(str_and_int[1])){
                     String key = str_and_int[0];
                     int value = Integer.parseInt(str_and_int[1]);
 
                     if(!(value <= 6) || !(value >= 0)){
-                        cardDeck.put("Invalid cost for: " + key, null);
-
-
+                        invalid_cards.add(line); //adds invalid card to invalid_card list
                     }
+
                     if (cardDeck.containsKey(key)) {
-                        //updates existing values of repeated keys
-                        cardDeck.put(key, cardDeck.get(key) + value);
+                        cardDeck.put(key, cardDeck.get(key) + value); //updates existing values of repeated keys
                     } else {
-                        //add new keys
-                        cardDeck.put(key, value);
+                        cardDeck.put(key, value);  //add new keys
                     }
                 }
             }
-
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -70,9 +87,8 @@ public class SlayTheSpire {
                 ex.printStackTrace();
             }
         }
-        return cardDeck;
+        return true;
     }
-
     /**
      * Helper method for readTxtFile method.
      * Checks if the given string is an integer.
@@ -93,14 +109,44 @@ public class SlayTheSpire {
         }
     }
 
+
+    public static void generateFile(boolean file_to_generate){
+
+        if(file_to_generate == false){
+
+            //generate VOID file
+        }
+
+        if(file_to_generate == true){
+
+            //generate REPORT (deck id, total cost of cards in deck, histogram of cards in deck
+        }
+    }
     public static void main(String[] args) {
 
         String filePath = "/Users/sophiabrix/Desktop/Java Programs/SlayTheSpireDeckCostTally/src/Deck.txt";
 
+        //SCANNER TO TAKE IN INPUT
+        boolean file_to_generate = readTxtFile(filePath);
 
-        HashMap<String, Integer> cardDeck = readTxtFile(filePath);
+        generateFile(file_to_generate);
 
         System.out.println(cardDeck);
     }
 
 }
+
+/*
+Things to check for:
+- invalid cost value
+- empty names
+- card that's only spaces or a tab
+-Must include invalid cards in the report!!!^^^ & ignore the values
+
+- 10 invalid cards
+- more than 1000 cards
+-Produce a VOID report!!!^^^ (contains the message: VOID
+
+AFTER ALL FILES HAVE BEEN GENERATED THE HASHMAP AND LIST SHOULD BE CLEARED!!!
+
+ */
